3f485d235e53b8b485b6e14519655f5c252f3a49
Susanna Albrecht <susanna.albrecht@outlook.com>
2017-10-29 11:48:24 +0100

    Antworten auf Bist du wissbegierig? hinzugefügt

--- Studienberatung.java
+++ Studienberatung.java
@@ -4,10 +4,20 @@ public class Studienberatung{
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hast du Abitur?");
 		String antwort = scanner.nextLine();
-		if(antwort.equalsIgnoreCase("nein"){
+		if(antwort.equalsIgnoreCase("nein")){
 			System.out.println("Hast du Fachabitur?");
-		else if(antwort.equalsIgnoreCase("ja"){
+		}
+		else if(antwort.equalsIgnoreCase("ja")){
 			System.out.println("Bist du wissbegierig?");
+			antwort = scanner.nextLine();
+			if(antwort.equalsIgnoreCase("ja")){
+				System.out.println("Auf jeden Fall studieren");
+				System.exit(0);
+			}
+			else if(antwort.equalsIgnoreCase("nein")){
+				antwort.equalsIgnoreCase("Nicht studieren");
+				System.exit(0);
+			}
 		}
 	}
 }
